9061665854b7aef2a376e8a8cd2a7d56242b3a7e
Jeverson misael da cruz filho <jeversonmisaldacruzfilho@gmail.com>
2024-07-21 12:51:47 -0300

    FEAT: New Post rout
    On this commit i create the route for model of the vehicles, and i remove brand_id for the bank data beacause its a redundance causing error, and in the server.ts i insert the rout

---
+++ src/routers/create-model.ts
@@ -0,0 +1,31 @@
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import z from "zod";
+import { prisma } from "../lib/prisma";
+
+export async function createModel(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    "/model",
+    {
+      schema: {
+        body: z.object({
+          name: z.string().min(3).max(25),
+          brandId: z.string(),
+          typeVehicleId: z.string(),
+        }),
+      },
+    },
+    async (request) => {
+      const { name, brandId, typeVehicleId } = request.body;
+      const sendModelvehicle = await prisma.model.create({
+        data: {
+          name,
+          brandId,
+          typeVehicleId,
+        },
+      });
+
+      return { modelId: sendModelvehicle.id };
+    }
+  );
+}
--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,8 @@ import {
 } from "fastify-type-provider-zod";
 import { createBrand } from "./routers/create-brand";
 import { createTypeVehicle } from "./routers/create-typeVehicle";
+import { create } from "domain";
+import { createModel } from "./routers/create-model";
 
 const app = fastify();
 
@@ -20,6 +22,7 @@ app.setSerializerCompiler(serializerCompiler);
 app.register(createColor);
 app.register(createBrand);
 app.register(createTypeVehicle);
+app.register(createModel);
 app
   .listen({
     port: 3333,
