9234bff5e9a3dbdc8cd432ac02e793a031e53de3
Jeverson misael da cruz filho <jeversonmisaldacruzfilho@gmail.com>
2024-07-30 11:18:01 -0300

    Feat: Creating rout
    I started a creating a new rout for the colors, when i will list the  espific color

---
+++ src/routers/color/detail-color.ts
@@ -0,0 +1,18 @@
+import type { FastifyInstance } from 'fastify'
+
+export async function detailColor(app: FastifyInstance) {
+  app.withTypeProvider<fastify.ZodTypeProvider>().get('/color/:colorId',
+{
+
+
+  
+}
+
+
+
+
+
+
+
+  )
+}
